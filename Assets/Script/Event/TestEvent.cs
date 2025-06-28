9280d2cf6d872337a9cdcfe9c4d9be5d42913142
gteb-XGe <1252283998@qq.com>
2025-06-28 09:59:36 +0800

    附身触发基础

---
+++ Assets/Script/Event/TestEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEvent :  MonoBehaviour ,BaseEvent
+{
+   public void StartEvent()
+    {   
+        Debug.Log("111");
+    }
+}
---
+++ Assets/Script/Event/TestEventt.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEventt : MonoBehaviour, BaseEvent
+{
+  public void StartEvent()
+    {
+        Debug.Log("222");
+    }
+
+  
+}
