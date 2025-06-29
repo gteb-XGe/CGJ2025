046d851a0c6b93339438e3626d01c96c398f5cd1
gr1se0 <18868811485@163.com>
2025-06-29 10:51:32 +0800

    Text单例

--- Assets/Script/Code_CRT/Globals.cs
+++ Assets/Script/Code_CRT/Globals.cs
@@ -8,4 +8,22 @@ public class Globals : MonoBehaviour
     public const string BGM_Start = "Audio/Test/1";
     public const string BGM_Main = "Audio/Test/2";
     public const string BGM_MainLoop = "Audio/Test/3 loop";
+
+    [Header("lev1ÒôÆµÎÄ¼þ")]
+    public const string BGM_Level_01_Amb_AirConditioner = "Audio/lev1/Level_01_Amb_AirConditioner";
+    public const string BGM_Level_01_Amb_Rain_Inside = "Audio/lev1/Level_01_Amb_Rain_Inside";
+    public const string BGM_Level_01_Amb_Rain_Outside = "Audio/lev1/Level_01_Amb_Rain_Outside";
+    public const string BGM_Level_01_Book_02 = "Audio/lev1/Level_01_Book_02";
+    public const string BGM_Level_01_Book_01 = "Audio/lev1/Level_01_Book_01";
+    public const string BGM_Level_01_ChairMove = "Audio/lev1/Level_01_ChairMove";
+    public const string BGM_Level_01_Clock_01 = "Audio/lev1/Level_01_Clock_01";
+    public const string BGM_Level_01_Clock_02 = "Audio/lev1/Level_01_Clock_02";
+    public const string BGM_Level_01_Clock_03 = "Audio/lev1/Level_01_Clock_03";
+    public const string BGM_Level_01_Clock_04 = "Audio/lev1/Level_01_Clock_04";
+    public const string BGM_Level_01_Computer_Beep_01 = "Audio/lev1/Level_01_Computer_Beep_01";
+    public const string BGM_Level_01_Computer_Succeed = "Audio/lev1/Level_01_Computer_Succeed";
+    public const string BGM_Level_01_Dumbbell_Rolling = "Audio/lev1/Level_01_Dumbbell_Rolling";
+    public const string BGM_Level_01_Dumbbell_Stop = "Audio/lev1/Level_01_Dumbbell_Stop";
+    public const string BGM_Level_01_SmallFan = "Audio/lev1/Level_01_SmallFan";
+    public const string BGM_Level_01_WindowOpen = "Audio/lev1/Level_01_WindowOpen";
 }
